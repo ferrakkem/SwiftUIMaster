5b8adc0c06cf7c1fd71ccd3ca66c0d4a39162487
Md.Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2023-10-16 16:37:59 -0300

    HiKe
    Initial  commit of Hike Project

---
+++ Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ferrakkem Bhuiyan on 2023-10-16.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
