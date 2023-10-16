67bf66d7c3127f3c1e15946d7e893212949e491c
Md.Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2023-10-16 18:26:13 -0300

    CardView
    - Created Extension for Color
    - Modified CardView and BackgroundView

---
+++ Hike/Hike/Utility/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by Ferrakkem Bhuiyan on 2023-10-16.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customGrayDark = Color("ColorGrayDark")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalminLight = Color("ColorSalminLight")
+}
